774e641105a5279c6c1e45b1c752c85fad96cfe8
MichaelAllain <michaeldallain@gmail.com>
2018-06-19 15:18:22 -0700

    Remove unnecessary components

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,12 @@
 var record = document.querySelector('.record');
 var stop = document.querySelector('.stop');
 var soundClips = document.querySelector('.sound-clips');
-var canvas = document.querySelector('.visualizer');
 var mainSection = document.querySelector('.main-controls');
 
 // disable stop button while not recording
 
 stop.disabled = true;
 
-// visualiser setup - create web audio api context and canvas
-
-var audioCtx = new (window.AudioContext || webkitAudioContext)();
-var canvasCtx = canvas.getContext("2d");
 
 //main block for doing the audio recording
 
@@ -25,8 +20,6 @@ if (navigator.mediaDevices.getUserMedia) {
 
   var onSuccess = function(stream) {
     var mediaRecorder = new MediaRecorder(stream);
-
-    visualize(stream);
 
     record.onclick = function() {
       mediaRecorder.start();
@@ -59,6 +52,9 @@ if (navigator.mediaDevices.getUserMedia) {
       var clipLabel = document.createElement('p');
       var audio = document.createElement('audio');
       var deleteButton = document.createElement('button');
+
+      var predictBtn = document.createElement('button');
+      predictBtn.textContent = 'Get Prediction';
      
       clipContainer.classList.add('clip');
       audio.setAttribute('controls', '');
@@ -74,6 +70,7 @@ if (navigator.mediaDevices.getUserMedia) {
       clipContainer.appendChild(audio);
       clipContainer.appendChild(clipLabel);
       clipContainer.appendChild(deleteButton);
+      clipContainer.appendChild(predictBtn);
       soundClips.appendChild(clipContainer);
 
       audio.controls = true;
@@ -86,6 +83,11 @@ if (navigator.mediaDevices.getUserMedia) {
       deleteButton.onclick = function(e) {
         evtTgt = e.target;
         evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+      }
+
+      predictBtn.onclick = function(e) {
+        evtTgt = e.target;
+        
       }
 
       clipLabel.onclick = function() {
@@ -114,61 +116,3 @@ if (navigator.mediaDevices.getUserMedia) {
    console.log('getUserMedia not supported on your browser!');
 }
 
-function visualize(stream) {
-  var source = audioCtx.createMediaStreamSource(stream);
-
-  var analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
-
-  source.connect(analyser);
-  //analyser.connect(audioCtx.destination);
-
-  draw()
-
-  function draw() {
-    WIDTH = canvas.width
-    HEIGHT = canvas.height;
-
-    requestAnimationFrame(draw);
-
-    analyser.getByteTimeDomainData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-    canvasCtx.beginPath();
-
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
-
-
-    for(var i = 0; i < bufferLength; i++) {
- 
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
-
-      if(i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-    }
-
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-
-  }
-}
-
-window.onresize = function() {
-  canvas.width = mainSection.offsetWidth;
-}
-
-window.onresize();
